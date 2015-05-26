621764b86596e0c7e6cdd2a958409ea4d60b0c8f
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2015-05-26 12:45:44 -0300

    Criado Singleton para gerenciar opções de jogo.

---
+++ MC03/GameControlSingleton.swift
@@ -0,0 +1,22 @@
+//
+//  GameControlSingleton.swift
+//  MC03
+//
+//  Created by Bruno Omella Mainieri on 5/26/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import Foundation
+
+class GameControlSingleton: NSObject {
+    
+    static let sharedInstance = GameControlSingleton()
+    
+    var difficulty:Int = 0
+    
+    private override init(){
+    }
+    
+    
+    
+}
