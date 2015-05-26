23caf9fc434a93d009c7efae8860a6915a5dc779
Rubens <rubens.gondek@gmail.com>
2015-05-26 15:14:48 -0300

    Livros com jogos diferentes Começando o visual da view de Pontuação

--- MC03/ViewController02.swift
+++ MC03/ViewController02.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController02: UIViewController {
-    @IBOutlet var pageView: UIView!
+    @IBOutlet var pageView1: UIView!
+    
+    @IBOutlet weak var pageView2: UIView!
+    
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var lblText: UILabel!
     
@@ -27,23 +30,75 @@ class ViewController02: UIViewController {
     
     @IBAction func btnEasy(sender: UIButton) {
         lblText.text = ("EASY!!")
-        let gameView = self.storyboard?.instantiateViewControllerWithIdentifier("gameView") as! GameViewController
-        self.presentViewController(gameView, animated: true, completion: nil)
+        switch (sender.tag){
+        case 0:
+            let gameView = self.storyboard?.instantiateViewControllerWithIdentifier("gameView") as! GameViewController
+            self.presentViewController(gameView, animated: true, completion: nil)
+        case 1:
+            println("Game 2")
+        default:
+            println("Another Game")
+        }
     }
     @IBAction func btnMedium(sender: UIButton) {
         lblText.text = ("MEDIUM!!")
+        switch (sender.tag){
+        case 0:
+            let gameView = self.storyboard?.instantiateViewControllerWithIdentifier("gameView") as! GameViewController
+            self.presentViewController(gameView, animated: true, completion: nil)
+        case 1:
+            println("Game 2")
+        default:
+            println("Another Game")
+        }
     }
     @IBAction func btnHard(sender: UIButton) {
         lblText.text = ("HARD!!")
+        switch (sender.tag){
+        case 0:
+            let gameView = self.storyboard?.instantiateViewControllerWithIdentifier("gameView") as! GameViewController
+            self.presentViewController(gameView, animated: true, completion: nil)
+        case 1:
+            println("Game 2")
+        default:
+            println("Another Game")
+        }
     }
     
     @IBAction func book1Click(sender: UIButton) {
         let btn = sender as UIButton
-        lblTitle.text = (btn.titleForState(UIControlState.Normal))
-        UIView.animateWithDuration(0.7, animations: { () -> Void in
-            self.pageView.alpha = 1
-            self.pageView.transform = CGAffineTransformMakeTranslation(0, 175)
-        })
+        if self.pageView1.alpha == 0 {
+            lblTitle.text = (btn.titleForState(UIControlState.Normal))
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
+                self.pageView1.alpha = 1
+                self.pageView1.transform = CGAffineTransformMakeTranslation(0, 175)
+            })
+        }
+        else {
+            lblTitle.text = (btn.titleForState(UIControlState.Normal))
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
+                self.pageView1.alpha = 0
+                self.pageView1.transform = CGAffineTransformMakeTranslation(0, 0)
+            })
+        }
+    }
+    
+    @IBAction func book2Click(sender: UIButton) {
+        let btn = sender as UIButton
+        if self.pageView2.alpha == 0 {
+            lblTitle.text = (btn.titleForState(UIControlState.Normal))
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
+                self.pageView2.alpha = 1
+                self.pageView2.transform = CGAffineTransformMakeTranslation(0, 175)
+            })
+        }
+        else {
+            lblTitle.text = (btn.titleForState(UIControlState.Normal))
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
+                self.pageView2.alpha = 0
+                self.pageView2.transform = CGAffineTransformMakeTranslation(0, 0)
+            })
+        }
     }
 
     /*
